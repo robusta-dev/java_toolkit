183a532f35a93ea0d2b884afa4f7c606f8e29338
avi robusta <avi@robusta.dev>
2022-02-16 12:50:38 +0200

    added check for kind

--- src/java_toolkit/common/utils/remote_ns_cmd.py
+++ src/java_toolkit/common/utils/remote_ns_cmd.py
@@ -1,6 +1,19 @@
 import subprocess
 import typer
 
+KNOWN_NSENTER_ERRORS=\
+{
+    "reassociate to namespace 'ns/mnt' failed",
+    "reassociate to namespace 'ns/user' failed"
+}
+KNOWN_ERROR_MESSAGE ="Java-toolkit does not support the current cluster type.\n" \
+                     " If the cluster type is not minikube or kind please contact the robusta team!"
+
+def check_for_known_error(output: str):
+    for known_error in KNOWN_NSENTER_ERRORS:
+        if known_error in output:
+            return KNOWN_ERROR_MESSAGE
+    return output
 
 def run_command(cmd: str, verbose: bool):
     if verbose:
@@ -13,4 +26,5 @@ def run_command(cmd: str, verbose: bool):
 
 def run_cmd_in_proc_namespace(pid: int, command_to_run: str, verbose: bool):
     nsenter_cmd = f"nsenter -t {pid} -a {command_to_run}"
-    return run_command(nsenter_cmd, verbose)
+    output = run_command(nsenter_cmd, verbose)
+    return check_for_known_error(output)
